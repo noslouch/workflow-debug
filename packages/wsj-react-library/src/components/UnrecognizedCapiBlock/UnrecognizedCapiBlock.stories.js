cf0de9fb23e4560e737f2d426586b470f8989510
Kristen Cabrera <40896596+kris10cabrera@users.noreply.github.com>
2021-03-31 16:59:22 -0400

    feat: added Unrecognized CAPI Block component (#199)
    * remove gitkeeps
    
    * Create index.js
    
    * Create UnrecognizedCapiBlock.stories.js
    
    * chore: Auto commit changes 🛠
    
    Co-authored-by: kris10cabrera <kris10cabrera@users.noreply.github.com>
    Co-authored-by: Hrusikesh Panda <mrchief@users.noreply.github.com>

---
+++ packages/wsj-react-library/src/components/UnrecognizedCapiBlock/UnrecognizedCapiBlock.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import UnrecognizedCapiBlock from './index'
+
+export default {
+  title: 'Unrecognized Capi Block',
+  component: UnrecognizedCapiBlock,
+}
+
+export const Default = () => <UnrecognizedCapiBlock />
