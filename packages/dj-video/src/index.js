8faecefca9b09719900ecd2900b97d3f62803f2b
davetinnes <52978518+davetinnes@users.noreply.github.com>
2021-06-18 12:33:51 -0500

    export audio player (#385)

--- packages/dj-video/src/index.js
+++ packages/dj-video/src/index.js
@@ -1,2 +1,3 @@
 export { default as VideoPlayer } from './components/VideoPlayer';
+export { default as AudioPlayer } from './components/AudioPlayer';
 export { default as AtmosphericPlayer } from './components/AtmosphericPlayer';
